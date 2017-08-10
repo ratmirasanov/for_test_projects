5ed7d128cc3c3e17edc98792ea73d155dec8bbaa
ratmirasanov <asf1eh14i7d5fd3s2f3f>
2017-08-10 15:18:04 +0300

    Added test tasks.

---
+++ Task 1.py
@@ -0,0 +1,11 @@
+def max_func(argument1, argument2):
+
+    if argument1 > argument2:
+        max_value = argument1
+    else:
+        max_value = argument2
+
+    print("Max value is = " + str(max_value))
+    return max_value
+
+max_func(5, 10)
