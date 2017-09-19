5ca3233a9b2f2e1d05d6fa2833d9418d7a6484c9
ratmirasanov <ratmir.asanov.7@gmail.com>
2017-09-19 20:42:28 +0300

    Task #5 -- done.

---
+++ LearningProjects/task_5.py
@@ -0,0 +1,10 @@
+"""Task 5."""
+
+
+def reverse(argument1):
+    """Compute the reversal of a string."""
+
+    return argument1[::-1]
+
+
+print("The reversal of a string is = {}".format(reverse("Test string.")))
