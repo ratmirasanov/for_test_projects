ecbc8aeb0c0b9bb83daffcda3e91590a7011a01d
ratmirasanov <ratmir.asanov.7@gmail.com>
2017-11-02 16:21:31 +0200

    Fixed #5.

--- LearningProjects/task_5.py
+++ LearningProjects/task_5.py
@@ -7,20 +7,16 @@ import task_4
 def translate_func(argument1):
     """Translate string into "robber's language"."""
 
+    special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
     new_str = ""
     for letter in argument1:
-        if task_4.is_vowel(letter):
+        if task_4.is_vowel(letter) or letter in special_characters:
             new_str += letter
         else:
-            if letter != " ":
-                new_str += letter + "o" + letter
-            else:
-                new_str += " "
+            new_str += letter + "o" + letter
 
     return new_str
 
 
 print("Translated string into \"robber's language\" is: {}."
       .format(translate_func("I'm speaking Robber's language!")))
-
-# To Do: Нужно обрабатывать еще спецсимволы.
