5eff1edfe3dc62d9c5989da9bbe4481f04c2b64f
ratmirasanov <ratmir.asanov.7@gmail.com>
2017-11-02 16:44:16 +0200

    Fixed #8.

--- LearningProjects/task_8.py
+++ LearningProjects/task_8.py
@@ -7,9 +7,13 @@ import task_7
 def is_palindrome(argument1):
     """Recognize palindromes."""
 
-    return argument1 == task_7.reverse(argument1)
+    if str(argument1):
+        new_str = argument1.lower()
+        print(new_str)
+        new_str = ''.join(new_str.split())
+        return new_str == task_7.reverse(new_str)
+    else:
+        return False
 
 
 print("Is the string palindrome? {}.".format(is_palindrome("Aerate pet area")))
-
-# To Do: Нужно не обрабатывать пробелы.
